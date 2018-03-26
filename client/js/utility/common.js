a3cdf7fae5c628f4472bd9ec6b043a834bc84e9b
Michael Yang <ledongyang@gmail.com>
2018-03-26 11:17:32 -0400

    setup requestAnimationFrame and image loader

--- client/js/utility/common.js
+++ client/js/utility/common.js
@@ -23,3 +23,56 @@
     }
   }
 }())
+
+var loader = {
+  loaded: true,
+  loadedCount: 0,
+  totalCount: 0,
+  init: function() {
+    // check for sound support
+    var mp3Support, oggSupport
+    var audio = document.createElement('audio')
+    if (audio.canPlayType) {
+      // Currently canPlayType() returns: "", "maybe", or "probably"
+      mp3Support = "" !== audio.canPlayType('audio/mpeg')
+      oggSupport = "" !== audio.canPlayType('audio/ogg; codecs="vorbis"')
+    } else {
+      // The audio tag is not support
+      mp3Support = false
+      oggSupport = false
+    }
+    // Check for ogg then mp3, and finally set soundFileExtn to undefined
+    loader.soundFileExtn = oggSupport ? '.ogg' : mp3Support ? '.mp3' : undefined
+  },
+  loadImage: function(url) {
+    this.totalCount++
+    this.loaded = false
+    $('#loadingscreen').show()
+    var image = new Image()
+    image.src = url
+    image.onload = loader.itemLoaded
+    return image
+  },
+  soundFileExtn: '.ogg',
+  loadSound: function(url) {
+    this.totalCount++
+    this.loaded = false
+    $('#loadingscreen').show()
+    var audio = new Audio()
+    audio.src = url + loader.soundFileExtn
+    audio.addEventListener('canplaythrough', loader.itemLoaded, false)
+    return audio
+  },
+  itemLoaded: function() {
+    loader.loadedCount++
+    $('#loadingmessage').html('Loaded ' + loader.loadedCount + ' of ' + loader.totalCount)
+    if (loader.loadedCount >= loader.totalCount) {
+      loader.loaded = true
+      $('#loadingscreen').hide()
+      if (loader.onload) {
+        loader.onload()
+        loader.onload = undefined
+      }
+    }
+  }
+}
